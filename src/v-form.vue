d2a5f145a43784507c7e06d88ad2e4546a00a3c5
Erik Antonio Alarcon Pinedo <ealarcop@emeal.nttdata.com>
2023-11-01 16:53:19 -0500

    x

--- src/v-form.vue
+++ src/v-form.vue
@@ -40,6 +40,7 @@ export default {
 
       if (el.children[1]) {
         console.log(el.children[0].textContent);
+        console.log(el.children[0]);
         console.log(el.children[0].offsetHeight);
         //se obtiene el alto del headr
         h = h - el.children[0].offsetHeight;
