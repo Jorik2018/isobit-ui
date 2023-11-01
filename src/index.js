b77e831f5ad38da5c55e3d00044401db50f2607d
Erik Antonio Alarcon Pinedo <ealarcop@emeal.nttdata.com>
2023-11-01 14:46:55 -0500

    x

--- src/index.js
+++ src/index.js
@@ -168,10 +168,10 @@ let resize = function () {
 			var event = new Event("parentResize", { bubbles: true });
 			event.height = h;
 			p.dispatchEvent(event);
-/*if(p.children[0]){
-	h = h - p.children[0].offsetHeight - 0;
+if(p.children[0]){
+	h = h - p.children[0].offsetHeight;
 			p.style.height = h + 'px';
-}*/
+}
 			//pass body panel it must have a form > v-datatable
 			if (p.children[1]) {
 				var body = p.children[1];
